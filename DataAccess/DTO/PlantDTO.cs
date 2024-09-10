84424437fcde24958540ceb7bb44dd39c69ca9b3
+Tinh Nguyễn _ <thunguyen2572004@gmail.com>
2024-09-10 21:39:26 +0700

    API CREATE PLANT+UPDATE PLANT

---
+++ DataAccess/DTO/PlantDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class PlantDTO
+    {
+        public int PlantId { get; set; }
+        public int UserId { get; set; }
+        public string PlantName { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = null!;
+        public int? Stock { get; set; }
+        public int? Status { get; set; }
+    }
+}
