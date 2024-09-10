21802cc8150f3fc2c08b7ff719cacee55c9097ef
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-09-10 17:42:54 +0700

    Update Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import Header from './components/Header'
+import Footer from './components/Footer'
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
           <Route path='/dashboard' element={<Dashboard/>}/>
  
     </Routes>
+    <Footer/>
    </BrowserRouter>
   )
 }
