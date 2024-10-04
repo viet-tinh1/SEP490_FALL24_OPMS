74e426feacc794fff793b8f3e49e0c5a789bd2cc
NguyenLeHoai <www.dragonwatervnvn@gmail.com>
2024-10-04 07:33:26 +0700

    HoaiNL3

---
+++ DataAccess/DTO/ChangePasswordDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class ChangePasswordDTO
+    {
+        public int Id { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
