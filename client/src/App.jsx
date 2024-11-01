55f3a8c60bc4bcafbbb62b172fe97058adf53a6c
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-11-01 08:55:33 +0700

    cập nhập App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,41 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import About from './pages/About'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import ProductDetail from './pages/ProductDetail'
-import Payment from './pages/Payment'
-import ProductSeller from './pages/ProductSeller'
-import ProductEdit from './pages/ProductEdit'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import ProductDetail from "./pages/ProductDetail";
+import Payment from "./pages/Payment";
+import ProductSeller from "./pages/ProductSeller";
+import ProductEdit from "./pages/ProductEdit";
+import ProductUpdate from "./pages/ProductUpdate";
+import DiscountEdit from "./pages/DiscountEdit";
+import DiscountUpdate from "./pages/DiscountUpdate";
 export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-        
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/sign-in' element={<SignIn/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
-          <Route path='/product' element={<Product/>}/>
-          <Route path='/payment' element={<Payment/>}/>
-
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/productdetail' element={<ProductDetail/>}/>
-          <Route path='/producsSeller' element={<ProductSeller/>}/>
-          <Route path='/ProductEdit' element={<ProductEdit/>}/>
-
-
-    </Routes>
-    <Footer/>
-   </BrowserRouter>
-  )
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/productdetail" element={<ProductDetail />} />
+        <Route path="/producsSeller" element={<ProductSeller />} />
+        <Route path="/ProductEdit" element={<ProductEdit />} />
+        <Route path="/ProductUpdate" element={<ProductUpdate />} />
+        <Route path="/DiscountEdit" element={<DiscountEdit />} />
+        <Route path="/DiscountUpdate" element={<DiscountUpdate />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
