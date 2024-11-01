59ca2d0ee03ac26cff49e07054719a4729f6a80b
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-11-01 08:56:33 +0700

    Cập nhập DashBoard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,42 +1,46 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
-import DashPosts from '../components/DashPosts';
-import DashUsers from '../components/DashUsers';
-import DashComments from '../components/DashComments';
-import Dashproduct from '../components/Dashproduct';
-import DashRegisterSeller from '../components/DashRegisterSeller';
-import DashVerifyProduct from '../components/DashVerifyProduct';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashComments from "../components/DashComments";
+import Dashproduct from "../components/Dashproduct";
+import DashRegisterSeller from "../components/DashRegisterSeller";
+import DashVerifyProduct from "../components/DashVerifyProduct";
+import DashDiscount from "../components/DashDiscount";
 export default function Dashboard() {
   const location = useLocation();
-  const [tab, setTab] = useState('');
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab');
+    const tabFromUrl = urlParams.get("tab");
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
   }, [location.search]);
   return (
-    <div className='min-h-screen flex flex-col md:flex-row'>
-      <div className='md:w-56'>
+    <div className="min-h-screen flex flex-col md:flex-row overflow-y-auto">
+      <div className="md:w-55 overflow-y-auto ">
         {/* Sidebar */}
-        <DashSidebar />
+        <DashSidebar/>
       </div>
       {/* profile... */}
-      {tab === 'profile' && <DashProfile />}
+      {tab === "profile" && <DashProfile />}
       {/* posts... */}
-      {tab === 'posts' && <DashPosts />}
+      {tab === "posts" && <DashPosts />}
       {/* users */}
-      {tab === 'users' && <DashUsers />}
+      {tab === "users" && <DashUsers />}
       {/* comments  */}
-      {tab === 'comments' && <DashComments />}
+      {tab === "comments" && <DashComments />}
       {/* dashboard Product */}
-      {tab === 'product' && <Dashproduct />}
+      {tab === "product" && <Dashproduct />}
       {/* dashboard Product */}
-      {tab === 'DashVerifyProduct' && <DashVerifyProduct />}
-      {tab === 'DashRegisterSeller' && <DashRegisterSeller />}
+      {tab === "DashVerifyProduct" && <DashVerifyProduct />}
+      {/* DashRegister Seller */}
+      {tab === "DashRegisterSeller" && <DashRegisterSeller />}
+      {/* Dash Discount */}
+      {tab === "DashDiscount" && <DashDiscount />}
     </div>
   );
 }
