7b4071b74d42fce4fb9c61e9442a7a10944a5bc3
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-11-01 08:56:03 +0700

    cập nhập DashSideBar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,4 @@
 import { Sidebar } from "flowbite-react";
-
 import {
   HiUser,
   HiArrowSmRight,
@@ -9,13 +8,12 @@ import {
   HiChartPie,
 } from "react-icons/hi";
 import { AiOutlineAppstore } from "react-icons/ai";
-
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-
 export default function DashSidebar() {
   const location = useLocation();
   const [tab, setTab] = useState("");
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -23,10 +21,13 @@ export default function DashSidebar() {
       setTab(tabFromUrl);
     }
   }, [location.search]);
+
   return (
-    <Sidebar className="w-full md:w-56">
+    <Sidebar className="w-full md:w-55 overflow-y-auto">
       <Sidebar.Items>
-        <Sidebar.ItemGroup className="flex flex-col gap-1">
+        <Sidebar.ItemGroup className="flex flex-col gap-2 py-2 ">
+          {" "}
+          {/* Adjusted gap and padding */}
           <Link to="/dashboard?tab=dash">
             <Sidebar.Item
               active={tab === "dash" || !tab}
@@ -36,7 +37,6 @@ export default function DashSidebar() {
               Dashboard
             </Sidebar.Item>
           </Link>
-
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
@@ -47,7 +47,6 @@ export default function DashSidebar() {
               Hồ sơ
             </Sidebar.Item>
           </Link>
-
           <Link to="/dashboard?tab=posts">
             <Sidebar.Item
               active={tab === "posts"}
@@ -57,46 +56,51 @@ export default function DashSidebar() {
               Bài Viết
             </Sidebar.Item>
           </Link>
-
-          <>
-            <Link to="/dashboard?tab=users">
-              <Sidebar.Item
-                active={tab === "users"}
-                icon={HiOutlineUserGroup}
-                as="div"
-              >
-                Quản lý tài khoản
-              </Sidebar.Item>
-            </Link>
-            <Link to="/dashboard?tab=comments">
-              <Sidebar.Item
-                active={tab === "comments"}
-                icon={HiAnnotation}
-                as="div"
-              >
-                Quản lý bình luận
-              </Sidebar.Item>
-            </Link>
-            <Link to="/dashboard?tab=product">
-              <Sidebar.Item
-                active={tab === "product"}
-                icon={AiOutlineAppstore}
-                as="div"
-              >
-                Quản Lý Sản phẩm
-              </Sidebar.Item>
-            </Link>
-            <Link to="/dashboard?tab=DashVerifyProduct">
-              <Sidebar.Item
-                active={tab === "product"}
-                icon={AiOutlineAppstore}
-                as="div"
-              >
-                Duyệt Sản Phẩm
-              </Sidebar.Item>
-            </Link>
-          </>
-
+          <Link to="/dashboard?tab=users">
+            <Sidebar.Item
+              active={tab === "users"}
+              icon={HiOutlineUserGroup}
+              as="div"
+            >
+              Quản lý tài khoản
+            </Sidebar.Item>
+          </Link>
+          <Link to="/dashboard?tab=comments">
+            <Sidebar.Item
+              active={tab === "comments"}
+              icon={HiAnnotation}
+              as="div"
+            >
+              Quản lý bình luận
+            </Sidebar.Item>
+          </Link>
+          <Link to="/dashboard?tab=product">
+            <Sidebar.Item
+              active={tab === "product"}
+              icon={AiOutlineAppstore}
+              as="div"
+            >
+              Quản Lý Sản phẩm
+            </Sidebar.Item>
+          </Link>
+          <Link to="/dashboard?tab=DashVerifyProduct">
+            <Sidebar.Item
+              active={tab === "DashVerifyProduct"}
+              icon={AiOutlineAppstore}
+              as="div"
+            >
+              Duyệt Sản Phẩm
+            </Sidebar.Item>
+          </Link>
+          <Link to="/dashboard?tab=DashDiscount">
+            <Sidebar.Item
+              active={tab === "DashDiscount"}
+              icon={AiOutlineAppstore}
+              as="div"
+            >
+              Mã giảm giá
+            </Sidebar.Item>
+          </Link>
           <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Đăng xuất
           </Sidebar.Item>
