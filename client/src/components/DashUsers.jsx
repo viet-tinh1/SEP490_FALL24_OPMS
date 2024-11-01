ffced830239fac6a8b8f02448c4649a031edf7a3
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-11-01 08:56:18 +0700

    cập nhập DashUser

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -152,18 +152,30 @@ export default function DashUsers() {
                 </Table.Cell>
                 <Table.Cell className="text-center">{user.roles}</Table.Cell>
                 <Table.Cell>{user.address}</Table.Cell>
-                <Table.Cell className="text-center">
-                  {user.blocked ? (
-                    <TbLock
-                      className="text-red-500 text-2xl cursor-pointer hover:text-red-700"
-                      onClick={() => handleToggle(user)}
+                <Table.Cell>
+                  <label className="inline-flex items-center mb-5 cursor-pointer">
+                    {/* Conditionally render lock or lock-open icon and the label */}
+                    <span className="text-2xl  hover:underline cursor-pointer">
+                      {user.blocked ? (
+                        <>
+                          <TbLock className="inline-block mr-2 text-red-500" />
+                        </>
+                      ) : (
+                        <>
+                          <TbLockOpen className="inline-block mr-2 text-green-500" />
+                        </>
+                      )}
+                    </span>
+
+                    <input
+                      type="checkbox"
+                      className="sr-only peer"
+                      checked={user.blocked}
+                      onChange={() => handleToggle(user)} // Pass the specific user
                     />
-                  ) : (
-                    <TbLockOpen
-                      className="text-green-500 text-2xl cursor-pointer hover:text-green-700"
-                      onClick={() => handleToggle(user)}
-                    />
-                  )}
+                    <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+                  </label>
                 </Table.Cell>
               </Table.Row>
             ))}
