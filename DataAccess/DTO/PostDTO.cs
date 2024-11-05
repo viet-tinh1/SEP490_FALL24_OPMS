372c8414147705128402f38c6f8e7dda49cd1bef
+Tinh Nguyễn _ <thunguyen2572004@gmail.com>
2024-11-05 20:22:19 +0700

    fix api lock account add api post (forum)

---
+++ DataAccess/DTO/PostDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DTO
+{
+    public class PostDTO
+    {
+        public int PostId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string PostContent { get; set; } = null!;
+
+        public string? PostImage { get; set; }
+
+        public DateTime? Createdate { get; set; }
+    }
+    public class PostDTOU
+    {
+        public int PostId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string PostContent { get; set; } = null!;
+
+        public string? PostImage { get; set; }
+
+        public int? LikePost { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
